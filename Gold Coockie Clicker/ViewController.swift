190088adccb05694df5c3acfc80e78d73db4d17c
levaniii <levan.tcharuashvili.1@btu.edu.ge>
2022-02-26 18:17:10 +0400

    Initial Commit

---
+++ Gold Coockie Clicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gold Coockie Clicker
+//
+//  Created by Levan Charuashvili on 26.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
